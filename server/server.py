07780e39499f1c6675ffe47a4be2f8abbb065145
Kirchikov Ivan <88023706+KABA4EEGGG@users.noreply.github.com>
2024-07-07 13:51:02 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,15 +1,11 @@
 import http.server
 import socketserver
 
-# Эта переменная нужна для обработки запросов клиента к серверу.
+
 
 handler = http.server.SimpleHTTPRequestHandler
 
-# Тут мы указываем, что сервер мы хотим запустить на порте 1234. 
-# Постарайтесь запомнить эти сведения, так как они нам очень пригодятся в дальнейшем, при работе с docker-compose.
+ 
 
 with socketserver.TCPServer(("", 7070), handler) as httpd:
-
-    # Благодаря этой команде сервер будет выполняться постоянно, ожидая запросов от клиента.
-
-   httpd.serve_forever()
+    httpd.serve_forever()
